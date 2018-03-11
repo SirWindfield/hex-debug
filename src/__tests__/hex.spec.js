3a42a8a7100eca57f255f9842c81660e4eb233f8
Sven Lechner <sirwindfield@users.noreply.github.com>
2018-03-11 23:23:10 +0100

    test(hex): Fixed broken tests because of object parameter

--- src/__tests__/hex.spec.js
+++ src/__tests__/hex.spec.js
@@ -6,19 +6,22 @@ const content16 = Buffer.from('Thisissomerandom');
 describe('Options', () => {
   it('prints a table with default options', () => {
     const expected = '000000  54 68 69 73 69 73 73 6F 6D 65 72 61 6E 64 6F 6D  Thisissomerandom';
-    const received = hex(content16, undefined, undefined, true);
+    const config = { color: false };
+    const received = hex(content16, config);
     expect(received).toEqual(expected);
   });
 
   it('prints with custom columns', () => {
     const expected = '000000  54 68 69 73 69 73 73 6F 6D 65 62 73  Thisissomebs';
-    const received = hex(content12, 12, undefined, true);
+    const config = { columns: 12, color: false };
+    const received = hex(content12, config);
     expect(received).toEqual(expected);
   });
 
   it('prints without the map', () => {
     const expected = '000000  54 68 69 73 69 73 73 6F 6D 65 72 61 6E 64 6F 6D ';
-    const received = hex(content16, undefined, false, true);
+    const config = { map: false, color: false };
+    const received = hex(content16, config);
     expect(received).toEqual(expected);
   });
 });

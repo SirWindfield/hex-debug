af5d700d6717d1db74d036c13c12960085a9cac0
Sven Lechner <sirwindfield@users.noreply.github.com>
2018-03-14 12:07:23 +0100

    refactor(test): Changed test case names

--- src/__tests__/hex.spec.js
+++ src/__tests__/hex.spec.js
@@ -12,21 +12,21 @@ describe('Options', () => {
     expect(hex(Buffer.from(''))).toEqual('');
   });
 
-  it('prints a table with default options', () => {
+  it('returns a table with default options', () => {
     const expected = '000000  54 68 69 73 69 73 73 6F 6D 65 72 61 6E 64 6F 6D  Thisissomerandom';
     const config = { color: false };
     const received = hex(content16, config);
     expect(received).toEqual(expected);
   });
 
-  it('prints with custom columns', () => {
+  it('returns a table with custom columns', () => {
     const expected = '000000  54 68 69 73 69 73 73 6F 6D 65 62 73  Thisissomebs';
     const config = { columns: 12, color: false };
     const received = hex(content12, config);
     expect(received).toEqual(expected);
   });
 
-  it('prints without the map', () => {
+  it('returns a table without the map', () => {
     const expected = '000000  54 68 69 73 69 73 73 6F 6D 65 72 61 6E 64 6F 6D ';
     const config = { map: false, color: false };
     const received = hex(content16, config);
